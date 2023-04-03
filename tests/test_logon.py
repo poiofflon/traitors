b6512f83ca25bcbec795c2dd26b702ec9d74d7d3
offlon <paulolsson24@gmail.com>
2023-04-03 21:03:03 +0100

    test registering lots of players and handling case when player name already registered

--- tests/test_logon.py
+++ tests/test_logon.py
@@ -8,6 +8,32 @@ def client():
 
 
 @pytest.fixture()
+def logged_on_client_factory():
+    """ return a client that has logged and ready to play"""
+    from ..main import app
+    def client(player_name):
+        flask_test_client = app.test_client()
+        flask_test_client.post("/", data={"player_name": player_name})
+        return flask_test_client
+    return client
+
+
+def test_lots_of_player_register(logged_on_client_factory, registered_players):
+    for i in range(1000):
+        logged_on_client_factory(str(i))
+    assert len(registered_players) == 1000
+
+
+def test_player_name_already_registered(logged_on_client_factory, client, registered_players):
+    """ should reload the index / logon page and prompt for another name """
+    player_name = "any_name"
+    logged_on_client_factory(player_name)
+    response = client.post("/", data={"player_name": player_name})
+    assert response.status_code == 200
+    assert len(registered_players) == 1
+
+
+@pytest.fixture()
 def random_username():
     import random
     import string
