a7bfddc20d70f731b1ba08d83971254a2febb821
offlon <paulolsson24@gmail.com>
2023-04-04 04:15:50 +0100

    black isort flake8

--- tests/test_game_play.py
+++ tests/test_game_play.py
@@ -4,17 +4,20 @@ import pytest
 @pytest.fixture()
 def registered_players():
     from ..main import registered_players
+
     return registered_players
 
 
 @pytest.fixture()
 def logged_on_client_factory():
-    """ return a client that has logged and ready to play"""
+    """return a client that has logged and ready to play"""
     from ..main import app
+
     def client(player_name):
         flask_test_client = app.test_client()
         flask_test_client.post("/", data={"player_name": player_name})
         return flask_test_client
+
     return client
 
 
@@ -25,23 +28,27 @@ def game_clients_factory(logged_on_client_factory):
         for i in range(no_of_players):
             clients.append(logged_on_client_factory(i))
         return clients
+
     return game_clients
 
 
 @pytest.fixture()
 def players():
     from ..main import players
+
     return players
 
 
 @pytest.fixture()
 def votes():
     from ..main import votes
+
     return votes
 
 
 def test_game_play(logged_on_client_factory, game_clients_factory, registered_players, players, votes):
     import random
+
     from ..main import admin
 
     # player with admin name can start date
@@ -50,7 +57,7 @@ def test_game_play(logged_on_client_factory, game_clients_factory, registered_pl
     clients = game_clients_factory(no_of_players - 1)
     clients.append(admin_client)
     # minimum 1 player voted off per round
-    max_no_rounds = 20/1
+    # max_no_rounds = 20 / 1
     min_player_vote_off = 0
     max_player_vote_off = 0
 
@@ -63,22 +70,7 @@ def test_game_play(logged_on_client_factory, game_clients_factory, registered_pl
 
     # round 1, everyone chooses
     for client in clients:
-        response = client.post("/game", data={
-            "vote": str(random.choice(players))
-        })
+        response = client.post("/game", data={"vote": str(random.choice(players))})
         assert response.status_code == 200
     min_player_vote_off += 1
     max_player_vote_off += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tests/test_logon.py
+++ tests/test_logon.py
@@ -4,17 +4,20 @@ import pytest
 @pytest.fixture()
 def client():
     from ..main import app
+
     return app.test_client()
 
 
 @pytest.fixture()
 def logged_on_client_factory():
-    """ return a client that has logged and ready to play"""
+    """return a client that has logged and ready to play"""
     from ..main import app
+
     def client(player_name):
         flask_test_client = app.test_client()
         flask_test_client.post("/", data={"player_name": player_name})
         return flask_test_client
+
     return client
 
 
@@ -25,7 +28,7 @@ def test_lots_of_player_register(logged_on_client_factory, registered_players):
 
 
 def test_player_name_already_registered(logged_on_client_factory, client, registered_players):
-    """ should reload the index / logon page and prompt for another name """
+    """should reload the index / logon page and prompt for another name"""
     player_name = "any_name"
     logged_on_client_factory(player_name)
     response = client.post("/", data={"player_name": player_name})
@@ -37,22 +40,21 @@ def test_player_name_already_registered(logged_on_client_factory, client, regist
 def random_username():
     import random
     import string
-    return ''.join(random.choices(string.ascii_letters, k=5))
+
+    return "".join(random.choices(string.ascii_letters, k=5))
 
 
 @pytest.fixture()
 def registered_players():
     from ..main import registered_players
+
     return registered_players
 
 
 def test_logon(client, random_username, registered_players):
-    response = client.post("/", data={
-        "player_name": random_username
-    })
+    response = client.post("/", data={"player_name": random_username})
     # test redirects user to wait page after entering username
     assert response.status_code == 302
     assert response.headers["Location"] == "/wait"
     # test user registered to play
     assert random_username in registered_players
-
